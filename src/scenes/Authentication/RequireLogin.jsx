a0600502963a75dcbf386b3238903b7fb76e99ce
muh-arifulislam <arifibnenam@gmail.com>
2023-04-08 05:00:37 -0600

    all done

--- src/scenes/Authentication/RequireLogin.jsx
+++ src/scenes/Authentication/RequireLogin.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../firebase.init";
+import Spinner from "react-bootstrap/Spinner";
 const RequireLogin = ({ children }) => {
   const [user, loading] = useAuthState(auth);
   let location = useLocation();
   if (loading) {
-    return <div>loading...</div>;
+    return (
+      <div style={{height:'100vh'}} className="d-flex justify-content-center align-items-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+    );
   }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
